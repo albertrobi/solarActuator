bc4822733f0a2a8cd1a65649dfef30291bcc7113
Robert-PC\Robert <albertrobi87@gmail.com>
2018-10-14 18:27:50 +0300

    Try to add OTA

--- sketch_webserver/sketch_webserver.ino
+++ sketch_webserver/sketch_webserver.ino
@@ -2,6 +2,8 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+//#include <WiFiUdp.h>
+//#include <ArduinoOTA.h>
 
 #include "index.h" //Our HTML webpage contents with javascripts
 
@@ -23,7 +25,11 @@ void handleRoot() {
    server.send(200, "text/html", s); //Send web page
 }
 
-// Stats
+// variables for wifi update
+//bool ota_flag = true;
+//uint16_t time_elapsed = 0;
+ 
+// Motor variables
 int turnRight = 0;
 String motorTurningDirection = "Right";
 
@@ -103,6 +109,13 @@ const int readFeedBackD6 = D6;
      Serial.print("FeedBack counts:  "); Serial.println(feedBackCount); 
    }
 
+  void allowDeviceUpdate()
+  {
+//    server.send(200,"text/plain", "Setting flag...");
+//    ota_flag = true;
+//    time_elapsed = 0;
+  }
+
 
 String webPage = "";
 
@@ -123,16 +136,20 @@ void setup(void){
   digitalWrite ( keepOnHighD3, HIGH );
   digitalWrite ( keepOnHighD4, HIGH );
   digitalWrite ( sendFeedBackD5, HIGH );
-  
+
+//  pinMode(2, OUTPUT);
   delay(1000);
   Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
   Serial.println("");
   Serial.print("Connected to ");
@@ -143,6 +160,47 @@ void setup(void){
   if (mdns.begin("esp8266", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+//  ArduinoOTA.onStart([]() {
+//    Serial.println("Start");
+//  });
+//  ArduinoOTA.onEnd([]() {
+//    Serial.println("\nEnd");
+//  });
+//  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+//    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+//  });
+//  ArduinoOTA.onError([](ota_error_t error) {
+//    Serial.printf("Error[%u]: ", error);
+//    if (error == OTA_AUTH_ERROR) {
+//      Serial.println("Auth Failed");
+//    } else if (error == OTA_BEGIN_ERROR) {
+//      Serial.println("Begin Failed");
+//    } else if (error == OTA_CONNECT_ERROR) {
+//      Serial.println("Connect Failed");
+//    } else if (error == OTA_RECEIVE_ERROR) {
+//      Serial.println("Receive Failed");
+//    } else if (error == OTA_END_ERROR) {
+//      Serial.println("End Failed");
+//    }
+//  });
+//  ArduinoOTA.begin();
+//  Serial.println("Ready");
+//  Serial.print("IP address: ");
+//  Serial.println(WiFi.localIP());
   
   server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
   server.on("/setLED", handleLED);
@@ -152,6 +210,7 @@ void setup(void){
   server.on("/motorTurnRight", handleMotorTurnRight);
   server.on("/motorTurnLeft", handleMotorTurnLeft);
   server.on("/resetFeedBackCounter", resetFeedBackCounter);
+  server.on("/startUpdate", allowDeviceUpdate);
   
   server.begin();
   Serial.println("HTTP server started");
@@ -160,5 +219,16 @@ void setup(void){
 }
  
 void loop(void){
-     server.handleClient();
+//  if(ota_flag)
+//  {
+//    uint16_t time_start = millis();
+//    while(time_elapsed < 15000)
+//    {
+//      ArduinoOTA.handle();
+//      time_elapsed = millis()-time_start;
+//      delay(10);
+//    }
+//    ota_flag = false;
+//  }
+  server.handleClient();
 } 
